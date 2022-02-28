1cc07cee8caed4c281cd91845ab3f517f18e6190
linxiaohui <llinxiaohui@126.com>
2022-02-28 10:33:41 +0800

    分词忽略图片

--- note/chinese_analyser.py
+++ note/chinese_analyser.py
@@ -1,4 +1,5 @@
 import time
+import re
 
 from whoosh.analysis import Tokenizer, Token
 import thulac
@@ -12,6 +13,10 @@ class ChineseAnalyzer(Tokenizer):
                  keeporiginal=False, removestops=True,
                  start_pos=0, start_char=0, mode='', **kwargs):
         t = Token(positions, chars, removestops=removestops, mode=mode, **kwargs)
+        # 将图片以base64的方式按markdown语法插入后，显著降低了Note模型保存时分词的效率
+        # value为note的markdown格式；在分词时将图片的base64忽略
+        image_pattern = re.compile(r"!\[.+?\][(]data:image/png;base64,.+[)]")
+        value = image_pattern.sub('', value)
         seglist = thu.cut(value)
         for w, _ in seglist:
             t.original = t.text = w
